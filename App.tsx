fad0758c59083c091cff684cf186e503f41f7f5e
Fernando Medina <f.medina@paisanoscreando.com>
2021-06-30 15:19:08 -0300

    pinch

--- App.tsx
+++ App.tsx
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Dimensions, Image } from "react-native";
+import { StyleSheet } from "react-native";
+import {
+  PinchGestureHandler,
+  PinchGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
+const imgUrl =
+  "https://images.unsplash.com/photo-1624724587089-97dcda66676d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80";
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
+const { width, height } = Dimensions.get("window");
 export default function App() {
+  const scale = useSharedValue(1);
+  const focalX = useSharedValue(0);
+  const focalY = useSharedValue(0);
+
+  const pinchGestureEvent =
+    useAnimatedGestureHandler<PinchGestureHandlerGestureEvent>({
+      onActive: event => {
+        // console.log(event)
+        scale.value = event.scale;
+        focalX.value = event.focalX;
+        focalY.value = event.focalY;
+      },
+      onEnd: () => {
+        scale.value = withTiming(1);
+      },
+    });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      // transform: [{scale: scale.value}],
+      transform: [
+        { translateX: focalX.value },
+        { translateY: focalY.value },
+        { translateX: -width / 2 },
+        { translateY: -height / 2 },
+        { scale: scale.value },
+        { translateX: -focalX.value },
+        { translateY: -focalY.value },
+        { translateX: width / 2 },
+        { translateY: height / 2 },
+      ],
+    };
+  });
+
+  const focalPointStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: focalX.value }, { translateY: focalY.value }],
+    };
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PinchGestureHandler onGestureEvent={pinchGestureEvent}>
+      <Animated.View style={{ flex: 1 }}>
+        <AnimatedImage style={[{ flex: 1 }, rStyle]} source={{ uri: imgUrl }} />
+        <Animated.View style={[styles.focalPoint, focalPointStyle]} />
+      </Animated.View>
+    </PinchGestureHandler>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  focalPoint: {
+    ...StyleSheet.absoluteFillObject,
+    width: 20,
+    height: 20,
+    backgroundColor: "orange",
+    borderRadius: 10,
   },
 });
